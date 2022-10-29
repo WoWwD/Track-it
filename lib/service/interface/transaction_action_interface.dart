f4ffc3b8d4b4a2ed5d78987bad8db678816cdf57
Vova <kandrin2002@mail.ru>
2022-10-30 01:09:50 +0300

    удалил лишнее

--- lib/service/interface/transaction_action_interface.dart
+++
@@ -1,8 +0,0 @@
-abstract class TransactionAction {
-  void setAmount(double amount);
-  void setPrice(double price);
-  void setCost(double cost);
-  void setDateTime(DateTime dateTime);
-  void setNote(String note);
-  Future<void> addTransaction(String namePortfolio, String idCoin);
-}
